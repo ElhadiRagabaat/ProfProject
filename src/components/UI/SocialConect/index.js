2515e47ce8cce9bac93f6a2aba5adeb0fe56d755
elhadis <ahmad23soho@gmail.com>
2021-06-09 20:46:22 +0300

    socialmedialinks

--- src/components/UI/SocialConect/index.js
+++ src/components/UI/SocialConect/index.js
@@ -7,17 +7,17 @@ export default function SocialConect(props) {
     return (
         <div className="socialConect" style={props.style}>
             <span  className=" mrl-10" style={{ background: "ffffff",color:" grey"}}>Follow Me :</span>
-            <a className="socialLink " href="https://www.facebook.com/" target="_blank">
+            <a className="socialLink " href="https://www.facebook.com/alhadi.ahmed.50" target="_blank">
                 <FaFacebook/>
             </a>
             
-            <a className="socialLink mrl-10" href="https://www.linkedin.com/feed/"  target="_blank">
+            <a className="socialLink mrl-10" href="https://www.linkedin.com/in/elhadi-ragabaat-14831a194/"  target="_blank">
                 <FaLinkedin />
             </a>
             <a className="socialLink mrl-10" href="https://www.instagram.com/"  target="_blank">
                 <FaInstagram />
             </a>
-            <a  className=" mrl-10"href="#">
+            <a  className=" mrl-10"href="https://github.com/ElhadiRagabaat?tab=repositories">
                 <FaGithub/>
             </a>
         </div>
